c45733bc1382a3fdb250d977e2c1ba349b374876
Baedonghee <appleehdgml@gmail.com>
2021-05-28 22:05:20 +0900

    여러 컴포던트 방법 추가

--- base-syntax/src/App.svelte
+++ base-syntax/src/App.svelte
@@ -1,5 +1,10 @@
 <script>
-	let name = 'Donghee'
+	import ContactCard from './ContactCard.svelte';
+
+	let name = 'Donghee';
+	let title = '';
+	let image = '';
+	let description = '';
 	let age = 33;
 
 	// 따로 선언 안해줘도 됨
@@ -7,6 +12,7 @@
 
 	$: console.log(name);
 
+	// 이름이 DongDonghee 일 경우 실행
 	$: if (name === 'DongDonghee') {
 		console.log('It runs!')
 		age = 34;
@@ -39,3 +45,7 @@
 <input type="text" value={name} on:input={nameInput}>
 <!-- 양방향 -->
 <input type="text" bind:value={name}>
+<input type="text" bind:value={title}>
+<input type="text" bind:value={image}>
+<textarea rows="3" bind:value={description}></textarea>
+<ContactCard userName={name} userImage={image} jobTitle={title} {description}/>
