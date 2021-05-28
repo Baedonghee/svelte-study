b4aa75f140d788f817977dc34b9dbb7682741965
Baedonghee <appleehdgml@gmail.com>
2021-05-28 23:21:38 +0900

    key 이해

--- assignment/src/App.svelte
+++ assignment/src/App.svelte
@@ -1,30 +1,61 @@
 <script>
-	export let name;
+  let enteredPassword = "";
+  let passwordValidity = "short";
+
+  let passwords = [];
+
+  $: if (enteredPassword.trim().length < 5) {
+    passwordValidity = "short";
+  } else if (enteredPassword.trim().length > 10) {
+    passwordValidity = "long";
+  } else {
+    passwordValidity = "valid";
+  }
+
+  function confirmPassword() {
+    if (passwordValidity === "valid") {
+      passwords = [...passwords, enteredPassword];
+    }
+  }
+
+  function removePassword(index) {
+    passwords = passwords.filter((pw, idx) => {
+      return idx !== index;
+    });
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Assignment</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<p>Solve these tasks.</p>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+<ol>
+  <li>Add a password input field and save the user input in a variable.</li>
+  <li>
+    Output "Too short" if the password is shorter than 5 characters and "Too long" if it's longer than 10.
+  </li>
+  <li>
+    Output the password in a paragraph tag if it's between these boundaries.
+  </li>
+  <li>Add a button and let the user add the passwords to an array.</li>
+  <li>Output the array values (= passwords) in a unordered list (ul tag).</li>
+  <li>Bonus: If a password is clicked, remove it from the list.</li>
+</ol>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<input type="password" bind:value={enteredPassword} />
+
+<button on:click={confirmPassword}>Confirm Password</button>
+
+{#if passwordValidity === 'short'}
+  <p>Password is too short!</p>
+{:else if passwordValidity === 'long'}
+  <p>Password is too long!</p>
+{:else}
+  <p>Password: {enteredPassword}</p>
+{/if}
+
+<ul>
+  {#each passwords as pw, i (pw)}
+    <li on:click={removePassword.bind(this, i)}>{pw}</li>
+  {/each}
+</ul>
